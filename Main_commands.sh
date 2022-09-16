c8c4e96ecd9cb6997ec4171941396e685b1094d5
akhilpampana <pampana.akhil@gmail.com>
2022-09-16 17:29:08 -0500

    Update Main_commands.sh

--- Main_commands.sh
+++ Main_commands.sh
@@ -2,27 +2,36 @@
 # Script used to run GCTA/plink/bcftools for the analyses in the GREML-LDMS WGS 2022 paper
 # It contains the most important commands for performing GREML-LDMS from WGS
 # The Online Methods describe with greater details all the QC steps performed
+# Some steps are added from the original github page
 # Additional R scripts are also available in this git
 ########################################
 
 #### Variables
 ncpu=12 
-sample_list="/data/project/Arora_lab/akhil/TOPMED/BNP/NTproBNP/NTproBNP_14k/phenotypes/NTproBNP_14k.tsv" #List of samples with phenotypic information available
+sample_list="phenotypes/NTproBNP_14k.tsv" #List of samples with phenotypic information available
 
-###### Create BED files and perform first filtering on SVM variants and other QC metrics
+###############################################################################################################################################################
+# 			Create BED files and perform first filtering on SVM variants and other QC metrics - submitted as slurm job			      #
+###############################################################################################################################################################
+###### Create BED files and perform first filtering on SVM variants and other QC metrics - submitted as slurm job
 
 for i in  {1..22} ; do
-    	bcftools view -m2 -M2  -Ou  -i'FILTER="PASS"' --threads "$ncpu" -S "$sample_list" --force-samples /data/project/Arora_lab/akhil/TOPMED/COMPLETE/GENOTYPES/freeze.10a.chr"$i".pass_only.gtonly.minDP10.vcf.gz | 
-	bcftools  annotate --threads "$ncpu" -Ob -I +'%CHROM:%POS:%REF:%ALT' > /data/project/Arora_lab/akhil/TOPMED/BNP/NTproBNP/NTproBNP_14k/gwas/heritability/freeze10.14k.chr"$i".pass.bcf ;
-	plink --bcf /data/project/Arora_lab/akhil/TOPMED/BNP/NTproBNP/NTproBNP_14k/gwas/heritability/freeze10.14k.chr"$i".pass.bcf --maf 0.0001 --allow-extra-chr --keep-allele-order --geno 0.05 --hwe 0.000001 --threads ${ncpu} -make-bed --out freeze10.14k.chr"$i".0.0001 ;
+    	bcftools view -m2 -M2  -Ou  -i'FILTER="PASS"' --threads "$ncpu" -S "$sample_list" --force-samples freeze.10a.chr"$i".pass_only.gtonly.minDP10.vcf.gz | 
+	bcftools  annotate --threads "$ncpu" -Ob -I +'%CHROM:%POS:%REF:%ALT' > heritability/freeze10.14k.chr"$i".pass.bcf ;
+	plink --bcf gwas/heritability/freeze10.14k.chr"$i".pass.bcf --maf 0.0001 --allow-extra-chr --keep-allele-order --geno 0.05 --hwe 0.000001 --threads ${ncpu} -make-bed --out freeze10.14k.chr"$i".0.0001 ;
 done
 
-### Allele Frequency Calculations
+###############################################################################################################################################################
+# 								Allele Frequency Calculations								      #
+###############################################################################################################################################################
+
 for i in {1..22}; do
 plink2 --bfile freeze10.14k.chr${i}.0.0001 --freq --out freeze10.14k.chr${i}
 done
 
-### Subset variants as per freeze10 pca's generation encore
+###############################################################################################################################################################
+# 							Subset variants as per freeze10 pca's generation encore	    					      #
+###############################################################################################################################################################
 module load PLINK
 for i in {1..22}; do
 plink2 --bfile originial/freeze10.14k.chr${i}.0.0001 --extract variants to filter to --make-bed  --out plink_format/prunned_list_included_in_encore_pcs_generation/freeze10.14k.chr${i}.pruned
@@ -42,12 +51,14 @@ module load KING/2.1.2-foss-2016a
 king -b freeze10.14k.hqp_encore.bed --kinship    
 
 
-### plink to gds conversion to run king using genesis package in r
+###############################################################################################################################################################
+# 					Extraction and seperation on unrelated vs related using kinship score < 0.025 (king.kin)                 	      #
+###############################################################################################################################################################
+ # IN house R script used
 
-
-for i in {1..22}; do
-plink --bfile freeze10.14k.chr${i}.0.0001 --indep-pairwise 50 5 0.1 --out freeze10.14k.chr${i}
-done### Subset Variants to 4 bins [0.05],[0.01,0.05),[0.001,0.01),[0.0001,0.001)
+###############################################################################################################################################################
+# 					Subset Variants to 4 MAF bins [0.05],[0.01,0.05),[0.001,0.01),[0.0001,0.001)		                 	      #
+###############################################################################################################################################################
 #module load R => R
 require(data.table)
 category1 = data.frame() ## [0.0001,0.001)
@@ -78,7 +89,9 @@ write.table(category2,file="category2_0.001_0.01.csv",row.names=F,col.names=T,se
 write.table(category3,file="category3_0.01_0.05.csv",row.names=F,col.names=T,sep="\t",dec=".",quote=F)
 write.table(category4,file="category4_0.05.csv",row.names=F,col.names=T,sep="\t",dec=".",quote=F)
 
-## Category 1 ##merge not working 
+###############################################################################################################################################################
+# 							   category1 - [0.0001,0.001)						       			      #
+###############################################################################################################################################################
 
 for i in  {1..22} ; do 
 plink2 --bfile plink_format/freeze10.14k.chr${i}.0.0001 --extract subset_for_h2_calc/category1_0.0001_0.001/category1_0.0001_0.001.csv --make-bed --out subset_for_h2_calc/category1_0.0001_0.001/category1_chr${i}  ; 
@@ -104,15 +117,22 @@ for i in {1..22}; do
 plink --bfile cat1_chr${i} --extract cat1_${i}.prune.in --make-bed --out cat1_chr${i}_hqp
 done
 
-
+#/data/project/Arora_lab/akhil/TOPMED/BNP/NTproBNP/NTproBNP_14k/gwas/heritability/plink_format/prunned_list_included_in_encore_pcs_generation/unrelated_kinship_0.025.txt
 ls -l | grep hqp | grep bed | awk ' { print $9 } ' | sed 's|.bed||g' > merge
 module load PLINK/1.90-foss-2016a
 plink --bfile cat1_chr1_hqp --merge-list merge --make-bed --out cat1_chr_all_hqp
 
-## LD score calculation
+## Subset to unrelated individuals based on kinship < 0.025 calc
+for i in {1..22}; do
+plink --bfile category1_0.0001_0.001/cat1_chr${i}_hqp --keep /data/project/Arora_lab/akhil/TOPMED/BNP/NTproBNP/NTproBNP_14k/gwas/heritability/plink_format/prunned_list_included_in_encore_pcs_generation/unrelated_kinship_0.025.txt --make-bed --out category1_0.0001_0.001/cat1_chr${i}_hqp_unrel
+done
+
+## LD score calculation to do 
 gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 --bfile cat3_chr_all_hqp --ld-score-region 200 --out test --thread-num 100
 
-## Category 2
+###############################################################################################################################################################
+# 							   category2 - [0.001,0.01)						       			      #
+###############################################################################################################################################################
 
 for i in  {1..22} ; do 
 plink2 --bfile plink_format/freeze10.14k.chr${i}.0.0001 --extract subset_for_h2_calc/category2_0.001_0.01/category2_0.001_0.01.csv --make-bed --out subset_for_h2_calc/category2_0.001_0.01/category2_chr${i}  ; 
@@ -134,10 +154,16 @@ plink --bfile cat2_chr_all --indep-pairwise 50 5 0.1 --out cat2
 ## subset to prunned variants
 plink --bfile cat2_chr_all --extract cat2.prune.in --make-bed --out cat2_chr_all_hqp
 
+## subset to unrelated individuals
+plink --bfile category2_0.001_0.01/cat2_chr_all_hqp --keep /data/project/Arora_lab/akhil/TOPMED/BNP/NTproBNP/NTproBNP_14k/gwas/heritability/plink_format/prunned_list_included_in_encore_pcs_generation/unrelated_kinship_0.025.txt --make-bed --out category2_0.001_0.01/cat2_chr_all_hqp_unrel
+
+
 ## LD score calculation
 gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 --bfile cat3_chr_all_hqp --ld-score-region 200 --out test --thread-num 100
 
-### Category 3	
+###############################################################################################################################################################
+# 							   category3 - [0.01,0.05)						       			      #
+###############################################################################################################################################################
 for i in  {1..22} ; do 
 plink2 --bfile plink_format/freeze10.14k.chr${i}.0.0001 --extract subset_for_h2_calc/category3_0.01_0.05/category3_0.01_0.05.csv --make-bed --out subset_for_h2_calc/category3_0.01_0.05/category3_chr${i}  ; 
 done
@@ -158,10 +184,16 @@ plink --bfile cat3_chr_all --indep-pairwise 50 5 0.1 --out cat3
 ## subset to prunned variants
 plink --bfile cat3_chr_all --extract cat3.prune.in --make-bed --out cat3_chr_all_hqp
 
+## subset to unrelated individuals
+plink --bfile category3_0.01_0.05/cat3_chr_all_hqp --keep /data/project/Arora_lab/akhil/TOPMED/BNP/NTproBNP/NTproBNP_14k/gwas/heritability/plink_format/prunned_list_included_in_encore_pcs_generation/unrelated_kinship_0.025.txt --make-bed --out category3_0.01_0.05/cat3_chr_all_hqp_unrel
+
+
 ## LD score calculation
 gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 --bfile cat3_chr_all_hqp --ld-score-region 200 --out test --thread-num 100
 
-### Category 4
+###############################################################################################################################################################
+# 							 	  category4 - [0.05]						       			      #
+###############################################################################################################################################################
 for i in  {1..22} ; do 
 plink2 --bfile plink_format/freeze10.14k.chr${i}.0.0001 --extract subset_for_h2_calc/category4_0.05/category4_0.05.csv --make-bed --out subset_for_h2_calc/category4_0.05/category4_chr${i}  ; 
 done
@@ -182,27 +214,20 @@ plink --bfile cat4_chr_all --indep-pairwise 50 5 0.1 --out cat4
 ## subset to prunned variants
 plink --bfile cat4_chr_all --extract cat4.prune.in --make-bed --out cat4_chr_all_hqp
 
+## subset to unrelated individuals
+plink --bfile category4_0.05/cat4_chr_all_hqp --keep /data/project/Arora_lab/akhil/TOPMED/BNP/NTproBNP/NTproBNP_14k/gwas/heritability/plink_format/prunned_list_included_in_encore_pcs_generation/unrelated_kinship_0.025.txt --make-bed --out category4_0.05/cat4_chr_all_hqp_unrel
+
+
 ### LD score calculation
 gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 --bfile cat4_chr_all_hqp --ld-score-region 200 --out test1 --thread-num 100
 
 
 
-###### Computing IBD ######
-#Computing IBD segments. For computational reasons, it is recomended to prune/select common SNPs for faster runtime.
-module load KING/2.1.2-foss-2016a
-
-king -b ../category3_0.01_0.05/cat3_chr_all_hqp.bed --ibdseg --prefix ../category3_0.01_0.05/cat3_chr_all_hqp --cpus 4 --seglength 3
-king -b cat4_chr_all_hqp.bed --ibdseg --prefix cat4_chr_all_hqp --cpus 4 --seglength 3
+###############################################################################################################################################################
+# 							 		GRM CREATION						       			      #
+###############################################################################################################################################################
 
 
-king -b ${BED_file_merged_QC}.bed  \
-	--ibdseg \
-	--prefix ${BED_file_merged_QC_out_name} \
-	--cpus ${ncpu} \
-	--seglength 3
-
-
-###### GRM ######
 #Construct a GRM, extracting a list of variants (see 01_LD_bins.R to create LD bins from a bed of a MAF range)
 #GRM constructed by part (one part computed per script on an array job)
 
@@ -275,40 +300,10 @@ done
 for i in *unrelated.grm.bin ; do readlink -f "$i"  | cut -d'.' -f1-2 >>  path; done
 
 
-###### Filter variants wihin a MAF range ######
 
-plink \
-	--bfile ${BED_file_merged_QC} \
-	--maf 0.0001 \
-	--max-maf 0.001 \
-	--make-bed \
-	--threads ${ncpu} \
-	--out ${BED_file_MAF}
-
-
-###### SNP pruning ######
-#Select independant SNPs based on a different thresholds from a set of variants
-#Indep-pairwise parameters can be modified depending if the pruning is performed on common or rare variants 
-
-i={1..22}
-plink \
-	--bfile ${BED_file_merged_QC} \
-	--chr "$i" \
-	--extract ${list_variants_bin} \
-	--indep-pairwise 50 5 0.1 \
-	--out ${out_indep_var}_chr"$i" \
-	--threads ${ncpu}
-
-
-###### Compute PCs ######
-#Compute PC from GRM (GCTA) or from BED (plink2)
-
-GCTA \
-	--grm ${GRM_out} \
-	--threads ${ncpu} \
-	--pca 20 \
-	--out ${PCA_out}
-
+###############################################################################################################################################################
+# 							 		Compute PCs						       			      #
+###############################################################################################################################################################
 
 ##cat1
 ../../../softwares/gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 --grm category1_0.0001_0.001/test --pca 20 --threads 10 --out category1_0.0001_0.001/test
@@ -320,15 +315,10 @@ GCTA \
 ../../../softwares/gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 --grm test --pca 20 --threads 10 --out test
 
 
-plink2 \
-	--bfile ${BED_file_merged_QC} \
-	--extract ${list_variants_bin} \
-	--pca 20 approx \
-	--out ${PCA_out} \
-	--thread-num ${ncpu}
 
-
-###### REML ######
+###############################################################################################################################################################
+# 							 		REML							       			      #
+###############################################################################################################################################################
 #Run unconstrained REML from multiple GRMs (for GREML-LDMS), fitting PCs as quantitative covariates
 #REML-no-lrt doesn't calculate the reduced model
 
@@ -353,5 +343,33 @@ GCTA \
 gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 --grm test --reml-no-constrain --pheno phenotypes/Combined_4cohorts_NTproBNP_08222022.phen --qcovar category3_0.01_0.05/test.eigenvec --out test --thread-num 10 --reml-no-lrt
 ### cat 4
 gcta-1.94.1-linux-kernel-3-x86_64/gcta-1.94.1 --grm test --reml-no-constrain --pheno phenotypes/Combined_4cohorts_NTproBNP_08222022.phen --qcovar category4_0.05/test.eigenvec --out test --thread-num 10 --reml-no-lrt
+
+
+
+###### Computing IBD ###### Extra
+#Computing IBD segments. For computational reasons, it is recomended to prune/select common SNPs for faster runtime.
+#module load KING/2.1.2-foss-2016a
+
+#king -b ../category3_0.01_0.05/cat3_chr_all_hqp.bed --ibdseg --prefix ../category3_0.01_0.05/cat3_chr_all_hqp --cpus 4 --seglength 3
+#king -b cat4_chr_all_hqp.bed --ibdseg --prefix cat4_chr_all_hqp --cpus 4 --seglength 3
+
+
+#king -b ${BED_file_merged_QC}.bed  --ibdseg --prefix ${BED_file_merged_QC_out_name} --cpus ${ncpu} --seglength 3
+
+
+
+
+
+
+
+###############################################################################
+# 	Clumping and thresholding to get variants for conditional analysis    #
+###############################################################################
+module load PLINK/1.90-foss-2016a
+for i in {1..22}; do
+plink2 --bfile  ../../heritability/plink_format/originial/freeze10.14k.chr${i}.0.0001 --clump ../../gwas_orginal/GWAS_NTproBNP_updated_14k.txt.gz  --clump-p1 5e-9	 --clump-p2 0.01   --clump-r2 0.60 --clump-kb 250  --out conditional_analysis/snps_for_conditioning/freeze10.14k.chr${i}.0.0001
+done
+
+
 
 
